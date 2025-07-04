cdf72923c77be58a8d7f3566fe3673baf3149aa5
roberto-mazor <mazor.estudo@gmail.com>
2025-07-03 22:25:10 -0300

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
-      <h1>Ola mundo</h1>
+      <div className='card'>
+        <h1 className='card-title'>Rick Sanchez</h1>
+        <span className='tag'>Status: Vivo</span>
+        <span className='tag'>Espécie: Humana</span>
+        <span className='tag'>Origem: Terra c-137</span>
+        <img className='card-image' src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Image rick and morty" />
+      </div>
     </>
   )
 }
